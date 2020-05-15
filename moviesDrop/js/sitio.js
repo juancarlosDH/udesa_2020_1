416ecaf06fd04b50772ca115bf8cffc938a3859a
Juan Carlos Gonzalez <juancarlos@digitalhouse.com>
2020-05-15 14:14:55 -0300

    ciclos en las peliculas

--- moviesDrop/js/sitio.js
+++ moviesDrop/js/sitio.js
@@ -27,6 +27,44 @@ window.onload = function () {
             navegacion.style.display = 'none';
         }
     }
+
+    //agrego las pelis
+
+    let peliculas = document.querySelector('.peliculas')
+
+    let arrayPeliculas = [
+    { titulo : 'Guardianes de la Galaxia', imagen : 'img/movies/guardianesdelagalaxia.jpg' },
+    { titulo : 'Avengers', imagen : 'img/movies/avengers.jpg' },
+    { titulo : 'Inception', imagen : 'img/movies/inception.jpg' },
+    { titulo : 'StarWars 2', imagen : 'img/movies/starwars2.jpg' },
+    { titulo : 'Pirates del Caribe', imagen : 'img/movies/piratasdelcaribe.jpg' },
+    { titulo : 'Avatar', imagen : 'img/movies/avatar.jpg' },
+    { titulo : 'Dead Pool', imagen : 'img/movies/deadpool.jpg' }
+    ];
+
+    //recorro el array
+    for (let peli of arrayPeliculas) {
+
+        //aqui uso las comillas francesas
+        peliculas.innerHTML += `<div class="pelicula">
+        <div class="cont-imgs">
+        <a href="detalle_pelicula.html">
+            <img src="` + peli.imagen + `" alt="" class="peli-img">
+            </a>
+            <img src="img/premiere.png" alt="" class="banderin">
+        </div>
+        <div class="rating">
+            <i class="fas fa-star star-rating"></i>
+            <span class="text-rating">8.5</span>
+        </div>
+        <h3 class="peli-titulo">` + peli.titulo + `</h3>
+        <div>
+            <a href="editar_pelicula.html" class="btn btn-success">Editar</a>
+            <a href="eliminar_pelicula.html" class="btn btn-danger">Eliminar</a>
+        </div>
+        </div>`;
+
+    }
     
 
 
